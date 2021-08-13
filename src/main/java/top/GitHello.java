e68461d87acf7dbaedc99d23fffddeb90e0892a5
handsomeb-global <164833811@qq.com>
2021-08-13 17:13:51 +0800

    create branch

--- src/main/java/top/GitHello.java
+++ src/main/java/top/GitHello.java
@@ -4,5 +4,6 @@ public class GitHello {
     public static void main(String[] args) {
         System.out.println("hello idea_git!");
         System.out.println("更新");
+        System.out.println("create branch");
     }
 }
